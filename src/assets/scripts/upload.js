68aec064bf9a74ae484992196dddbb1c0e20d4a5
museekee <103117239+museekee@users.noreply.github.com>
2023-01-06 13:34:11 +0900

    업로드 만드는 중

--- src/assets/scripts/upload.js
+++ src/assets/scripts/upload.js
@@ -1,5 +1,12 @@
-document.getElementById("uploadBox").addEventListener("click", () => document.getElementById("videoFile").click())
-document.getElementById("videoFile").addEventListener("change", (e) => {
-    const file = document.getElementById("videoFile").files[0]
+const elems = {
+    uploadBox: document.getElementById("uploadBox"),
+    videoFile: document.getElementById("videoFile"),
+    uploadBtn: document.getElementById("uploadBtn"),
+}
+elems.uploadBtn.addEventListener("click", () => elems.videoFile.click())
+elems.videoFile.addEventListener("change", (e) => {
+    const file = elems.videoFile.files[0]
+    elems.uploadBox.setAttribute("class", "")
+    elems.uploadBtn.remove()
     console.log(file)
 })
